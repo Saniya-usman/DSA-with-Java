82132ad4ebd5a3031b9a07d86ed4c490d2046cd6
Saniya-uman <saniyabammanalli@gmail.com>
2026-08-28 18:29:50 +0530

    edited Kth largest element problem using selection sort

--- DSA/SelectionSort/KthLargestElement.java
+++ DSA/SelectionSort/KthLargestElement.java
@@ -3,14 +3,16 @@ package SelectionSort;
 public class KthLargestElement {
     static int KthLEISS(int[] arr, int k){
         int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n-i-1; j++) {
-                if (arr[j] > arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+        for (int i = 0; i < n-1; i++) {
+            int maxIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] < arr[maxIndex]) {
+                    maxIndex = j;
                 }
             }
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
         }
         return arr[n - k];
     }
