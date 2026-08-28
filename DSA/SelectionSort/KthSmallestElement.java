db35ebe56dbdccee07969af3f5396c4a070aa44d
Saniya-uman <saniyabammanalli@gmail.com>
2026-08-28 18:33:16 +0530

    edited Kth smallest element problem using selection sort

--- DSA/SelectionSort/KthSmallestElement.java
+++ DSA/SelectionSort/KthSmallestElement.java
@@ -2,19 +2,22 @@ package SelectionSort;
 
 public class KthSmallestElement {
     static int KthSMISS(int[] arr, int k){
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - 1 -i; j++) {
-                if (arr[j] < arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+       int n = arr.length;
+       for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j] > arr[minIndex]) {
+                    minIndex = j;
                 }
             }
-        }
-        return arr[k-1];
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+       }
+       return arr[k - 1];
     }
     public static void main(String[] args) {
         int[] arr = {7, 2, 5, 1, 9};
-        System.out.println(KthSMISS(arr, 3));
+        System.out.println(KthSMISS(arr, 2));
     }
 }
