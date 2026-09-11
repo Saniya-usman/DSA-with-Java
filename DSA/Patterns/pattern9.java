2d3f121e1b3a9d22d83b2250b22ca8a9a7055e98
Saniya-uman <saniyabammanalli@gmail.com>
2026-09-11 20:44:12 +0530

    solved pyramid and inverted pyramid patterns

---
+++ DSA/Patterns/pattern9.java
@@ -0,0 +1,21 @@
+package Patterns;
+
+public class pattern9 {
+    static void patternnine(int n){
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2*i-1; k++) {
+                System.out.print("* ");
+            }
+            
+              System.out.println( );
+        }
+      
+    }
+    public static void main(String[] args) {
+        patternnine(5);
+    }
+}
