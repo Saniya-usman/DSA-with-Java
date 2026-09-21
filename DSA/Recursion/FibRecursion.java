53a95a06ca367ed9b0316e4e93ebf67039d9ab4d
Saniya-uman <saniyabammanalli@gmail.com>
2026-09-21 21:00:19 +0530

    Fibonacii using recursion

---
+++ DSA/Recursion/FibRecursion.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class FibRecursion {
+    
+    static int fibo(int n){
+        if (n<2) {
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+    public static void main(String[] args) {
+        System.out.println(fibo(50));
+    }
+}
