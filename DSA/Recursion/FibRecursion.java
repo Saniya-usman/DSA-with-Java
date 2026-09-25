a07f6b77e35cd7019a1ffbed6f830e412b776e3a
Saniya-uman <saniyabammanalli@gmail.com>
2026-09-26 01:04:10 +0530

    learning time complexity and space complexity

--- DSA/Recursion/FibRecursion.java
+++ DSA/Recursion/FibRecursion.java
@@ -8,7 +8,11 @@ public class FibRecursion {
         }
         return fibo(n-1) + fibo(n-2);
     }
+    static int fibFormula(int n){
+        return (int)(Math.pow(((1+Math.sqrt(5))/2), n)/ Math.sqrt(5));
+    }
     public static void main(String[] args) {
-        System.out.println(fibo(50));
+        // System.out.println(fibo(50));
+        System.out.println(fibFormula(55));
     }
 }
